192d67f8d68e83779c63706db608991526f83c80
FahriAkbar02 <fapszoldick02@gmail.com>
2024-01-15 17:39:01 +0900

    ++++

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -35,12 +35,12 @@
                                     <div class="form-group row">
                                         <div class="col-sm-6 mb-3 mb-sm-0">
                                             <input type="password" name="password" class="form-control form-control-user
-                                            <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?>" autocomplete="off">
+                                            <?php if (session('errors.password')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.password') ?> Minimal 8 Angka.." autocomplete="off">
                                         </div>
 
                                         <div class="col-sm-6">
                                             <input type="password" name="pass_confirm" class="form-control form-control-user
-                                            <?php if (session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.repeatPassword') ?>" autocomplete="off">
+                                            <?php if (session('errors.pass_confirm')) : ?>is-invalid<?php endif ?>" placeholder="<?= lang('Auth.repeatPassword') ?> Minimal 8 Angka.." autocomplete="off">
                                         </div>
                                     </div>
                                     <button type="submit" class="btn btn-primary btn-user btn-block">
